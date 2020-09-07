c4c8ab92bd0d9728c204fdd43098375a04460b6e
Ashbojuk <ashbojuk@gmail.com>
2020-09-07 17:53:41 +0400

    Homework 6

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import ReactDom from 'react-dom';
 // import logo from './logo.svg';
 import './index.css';
 import './App.css';
-// import Hello from './home_demo/Hello';
-// import {Name} from './home_demo/Name.jsx';
-import Product from './home_demo/Product';
-// import Price from './home_demo/Price'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        
-        {/* <Hello code='JSX'/> */}
-        {/* <Name name='Ashkhen'/> */}
-        <Product name='bananas' price='2$' description='Fresh bananas'  />
+      
+       
       </header>
     </div>
   );
--- src/home_demo/Name.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Name=function(props){
-    return(
-    <p>I am {props.name}</p>
-    ) 
-}
-export {Name};
